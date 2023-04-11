428854329011c2488d8b6132c86b12260c634bd8
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-04-11 23:11:02 +0300

    Добавьте файлы проекта.

---
+++ Geometric_figure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Geometric_figure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
